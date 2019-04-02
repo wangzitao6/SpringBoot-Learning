f6c78d3d98c2b7c3941cbee3d9258d6f1bb117ee
wangzitao <1023185556@qq.com>
2019-04-02 17:35:53 +0800

    init

---
+++ src/main/java/com/wzt/SpringbootLearningApplication.java
@@ -0,0 +1,13 @@
+package com.wzt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearningApplication.class, args);
+	}
+
+}
